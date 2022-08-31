4336b1d197548f7ec14cb6dc9ff10c0593500b60
farhana <farhana17mar@gmail.com>
2022-08-31 16:46:13 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+    if (email === 'farhana17mar@gmail.com' && password === 'secret') {
+        window.location.href = "bank.html";
+    } else {
+        alert('invalid user');
+    }
+})
