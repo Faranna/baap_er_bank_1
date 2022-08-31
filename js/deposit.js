040ca039d9974e8726bb49e47ff989bb91a8e6bf
farhana <farhana17mar@gmail.com>
2022-08-31 19:52:01 +0600

    done

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,12 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
+    depositField.value = '';
+    if (isNaN(newDepositAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
 
     const depositTotalElement = document.getElementById('deposit-total');
 
@@ -15,5 +21,5 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
     const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
     balanceTotalElement.innerText = currentBalanceTotal;
-    depositField.value = '';
+
 })
